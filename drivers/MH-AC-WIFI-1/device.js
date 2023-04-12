9f2667d8e495d6344f36fca8494bc78d7a295d73
Marco <64333539+malu4@users.noreply.github.com>
2023-04-12 23:33:22 +0200

    Update device.js

--- drivers/MH-AC-WIFI-1/device.js
+++ drivers/MH-AC-WIFI-1/device.js
@@ -1,25 +1,32 @@
-'use strict';
+'use strict'
 
-const Homey = require('homey')
+const { Device } = require('homey')
 const AircoDevice = require('../airco/device.js')
 const acwm = require('acwm-api')
 
-class MHACWIFI1_Device extends AircoDevice {
+class MHACWIFI1_Device extends Device {
 
-	onInit() {
-		let settings = this.getSettings()
-		this.acwm = new acwm(settings.ip)
-		this.acwm.login(settings.username, settings.password)
-			.then(r => {
-				console.log(r)
-				this.interval = settings.interval
-				super.onInit()
-				this.updateAllValues()
-				this.log('MHACWIFI1_Device has been inited')
-			})
-			.catch(err => console.error(err))
-	}
+  async onInit() {
+    this.log('MHACWIFI1_Device has been inited')
+
+    let settings = this.getSettings()
+    this.acwm = new acwm(settings.ip)
+    this.acwm.login(settings.username, settings.password)
+      .then(r => {
+        console.log(r)
+        this.interval = settings.interval
+        this.registerReadyListener(this.onReady.bind(this))
+        this.updateAllValues()
+      })
+      .catch(err => console.error(err))
+  }
+
+  async onReady() {
+    // Place the code from the original onInit() method of AircoDevice class here.
+    // Make necessary changes to adapt it to the new SDK3 if needed.
+  }
 
 }
 
-module.exports = MHACWIFI1_Device;
+module.exports = MHACWIFI1_Device
+
