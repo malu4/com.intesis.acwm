fc2b9fefe28f301cdadca1b300484ec371da747f
Marco <64333539+malu4@users.noreply.github.com>
2023-04-12 23:26:12 +0200

    Update driver.js

--- drivers/airco/driver.js
+++ drivers/airco/driver.js
@@ -3,39 +3,45 @@
 const { Driver } = require('homey')
 const acwm = require('acwm-api')
 
-class AircoDevice extends Driver {
+class AircoDriver extends Driver {
 
   async onInit() {
-    this.log('AircoDevice has been inited')
+    this.log('AircoDriver has been inited')
+    this.on('pair', this.onPair.bind(this))
+  }
 
-    this.registerFlowCard('onoff', this.onPowerOnoff.bind(this))
-    this.registerFlowCard('thermostat_mode_mh', this.onThermostatMode.bind(this))
-    this.registerFlowCard('fan_rate_mh', this.onFanRate.bind(this))
-    this.registerFlowCard('vane_updown_position_mh', this.onVaneUpDownDirection.bind(this))
-    this.registerFlowCard('target_temperature', this.onSetpoint.bind(this))
+  async onPair(socket) {
+    this.log('Pairing started')
 
-    this.on('settings', this.onSettings.bind(this))
-    this.on('ready', () => {
-      this.setPollTimer(this.interval)
+    socket.emit('authorized')
+
+    socket.on('check', async (data, callback) => {
+      this.log('Checking', data)
+      try {
+        let airco = new acwm(data.ip)
+        let result = await airco.getInfo()
+        let login = await airco.login(data.username, data.password)
+
+        let device = {
+          name: result.ownSSID,
+          data: { id: result.sn },
+          settings: {
+            ip: data.ip,
+            username: data.username,
+            password: data.password,
+            interval: 10,
+            model: result.deviceModel,
+            serial: result.sn
+          }
+        }
+        callback(null, device)
+      } catch(err) {
+        this.error(err.error || err)
+        callback(err.error || err, null)
+      }
     })
   }
 
-  async registerFlowCard(capability, method) {
-    this.homey.flow.getDeviceTriggerCard(capability)
-      .registerRunListener(method)
-  }
-
-  async onSettings(newSettings) {
-    const oldSettings = this.getSettings()
-    const changedKeys = Object.keys(newSettings)
-
-    this.log(changedKeys)
-
-    // Rest of the onSettings code...
-  }
-
-  // Rest of the class methods...
 }
 
-module.exports = AircoDevice
-
+module.exports = AircoDriver
